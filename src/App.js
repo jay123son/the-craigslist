c6064b808db125f45f9c37e001350856fe780915
jay123son <youremail@domain.com>
2022-11-07 18:04:41 -0500

    craigslist

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Cards from './Components/Cards.js';
+function App() {
+  return (
+    <div className="App">
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Cards.js
@@ -0,0 +1,120 @@
+import salesData from "../Data.js"
+
+
+function Cards(){
+  return(
+    <div className="cardbox">
+      <div className="cards">
+      <h2>{`${salesData[0].image}`}</h2>
+      <p>{`${salesData[0].title}`}</p>
+        <p>{`${salesData[0].cost}`}</p>
+         
+            </div>
+
+
+
+
+            <div className="cards">
+      <h2>{`${salesData[1].image}`}</h2>
+      <p>{`${salesData[1].title}`}</p>
+        <p>{`${salesData[1].cost}`}</p>
+         
+            </div>
+
+
+
+
+
+
+
+            <div className="cards">
+      <h2>{`${salesData[2].image}`}</h2>
+      <p>{`${salesData[2].title}`}</p>
+        <p>{`${salesData[2].cost}`}</p>
+         
+            </div>
+
+
+
+
+
+            <div className="cards">
+      <h2>{`${salesData[3].image}`}</h2>
+      <p>{`${salesData[3].title}`}</p>
+        <p>{`${salesData[3].cost}`}</p>
+         
+            </div>
+
+
+            <div className="cards">
+      <h2>{`${salesData[4].image}`}</h2>
+      <p>{`${salesData[4].title}`}</p>
+        <p>{`${salesData[4].cost}`}</p>
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${salesData[5].image}`}</h2>
+      <p>{`${salesData[5].title}`}</p>
+        <p>{`${salesData[5].cost}`}</p>
+         
+            </div>
+
+
+
+
+            <div className="cards">
+      <h2>{`${salesData[6].image}`}</h2>
+      <p>{`${salesData[6].title}`}</p>
+        <p>{`${salesData[6].cost}`}</p>
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${salesData[7].image}`}</h2>
+      <p>{`${salesData[7].title}`}</p>
+        <p>{`${salesData[7].cost}`}</p>
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${salesData[8].image}`}</h2>
+      <p>{`${salesData[8].title}`}</p>
+        <p>{`${salesData[8].cost}`}</p>
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${salesData[9].image}`}</h2>
+      <p>{`${salesData[9].title}`}</p>
+        <p>{`${salesData[9].cost}`}</p>
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${salesData[10].image}`}</h2>
+      <p>{`${salesData[10].title}`}</p>
+        <p>{`${salesData[10].cost}`}</p>
+         
+            </div>
+            
+            
+
+            </div>
+            
+            
+  )}
+ 
+
+
+  export default Cards;
